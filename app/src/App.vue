c33687390be3d637679b9628d9686033ca226a90
Yarde <dawidkal66@gmail.com>
2020-04-04 22:35:12 +0200

    search bar

--- app/src/App.vue
+++ app/src/App.vue
@@ -18,28 +18,7 @@
 
         #ExpAtHome
       </div>
-
-      <v-spacer></v-spacer>
-      <v-toolbar light dense>
-        <v-app-bar-nav-icon @click="nav = !nav"></v-app-bar-nav-icon>
-
-        <v-toolbar-title>Title</v-toolbar-title>
-
-        <v-spacer></v-spacer>
-
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </v-toolbar>
-
+      
     </v-app-bar>
     <v-navigation-drawer
             :permanent="$vuetify.breakpoint.mdAndUp"
--- app/src/components/Tasks.vue
+++ app/src/components/Tasks.vue
@@ -1,6 +1,15 @@
 <template>
     <v-row>
-        <v-col cols="12" sm="12" md="6" lg="4" v-for="(task, t) in tasks" :key="t">
+        <v-col cols="12" sm="12" md="12" lg="12" >
+            <v-text-field 
+                outlined
+                label="Search tasks" 
+                prepend-inner-icon="mdi-magnify"
+                v-model="searchBar" 
+                hide-details="auto" 
+            ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="12" md="6" lg="4" v-for="(task, t) in get" :key="t">
             <v-card class="mt-9  pa-2">
                 <div class="d-flex flex-no-wrap "  @click="task.overlay = !task.overlay">
 
@@ -108,7 +117,8 @@
             comment:"",
             absolute: true,
             rating:4,
-            dialog: false
+            dialog: false,
+            searchBar: ""
         }),
         methods: {
             onScroll (e) {
@@ -120,5 +130,13 @@
             }
         },
         props: ['tasks'],
+        computed: {
+            get: function () {
+                return this.tasks.filter(tasks => {
+                    return tasks.name.toLowerCase().includes(this.searchBar.toLowerCase()) ||  tasks.info.toLowerCase().includes(this.searchBar.toLowerCase())
+                })
+            },
+        },
+        
     }
 </script>
